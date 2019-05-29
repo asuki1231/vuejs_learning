0cb9267cab56585887a7ee8f6f98e6136d3ffc7b
kosuge-takuhiro <kosuge_takuhiro@waku-2.com>
2019-05-29 18:15:41 +0900

    section-27

---
+++ vuejs_learning/other/changeComponent.js
@@ -0,0 +1,21 @@
+Vue.component('my-component-a', {
+  template: '<div class="my-component-a">component A</div>'
+});
+
+Vue.component('my-component-b', {
+  template: '<div class="my-component-b">component B</div>'
+});
+
+new Vue({
+  el: '#app',
+  data: {
+    componentTypes: ['my-component-a', 'my-component-b'],
+    current: 0
+  },
+  computed: {
+    component: function() {
+      //currentと一致するindexのコンポーネントに切り替える
+      return this.componentTypes[this.current];
+    }
+  }
+});
